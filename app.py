984e4a04b3c33b73701da9744a80054a3d39311c
LenSin3 <lenpk3@yahoo.co.uk>
2020-11-10 23:48:25 -0800

    Completed scrape route in app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # import necessary libraries
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_to_mars
 
@@ -13,6 +13,13 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_DB")
 def index():
     scraped_data = mongo.db.scraped_data.find_one()
     return render_template("index.html", scraped_data = scraped_data)
+
+@app.route("/scrape")
+def scraper():
+    scraped_data = mongo.db.scraped_data
+    scraped_data_list = scrape_to_mars.scrape()
+    scraped_data.update({}, scraped_data_list, upsert=True)
+    return redirect("/", code=302)
 
 
 if __name__ == "__main__":
